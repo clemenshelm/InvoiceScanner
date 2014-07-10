d3050619f84006fa9b3b73d9a511654a1f3e1391
Clemens Helm <clemens.helm@gmail.com>
2014-07-10 17:35:43 +0200

    Moved all quad logic into quad view.

--- app/layouts/invoice_shot_layout.rb
+++ app/layouts/invoice_shot_layout.rb
@@ -1,21 +1,10 @@
 class InvoiceShotLayout < MK::Layout
   include FullscreenWithNavbar
-
-  PIN_POSITIONS = [
-    [100, 150], [650, 150], [100, 900], [650, 900]
-  ]
-
   attr_reader :pins
 
   def layout
     add UIImageView, :invoice_image
     add QuadView, :quad
-    @pins = PIN_POSITIONS.each_with_index.map do |position, index|
-      add UIButton, "pin#{index}" do
-        frame [position, [20, 20]]
-        background_color QuadView::COLOR
-      end
-    end
   end
 
   def invoice_image_style
@@ -25,7 +14,6 @@ class InvoiceShotLayout < MK::Layout
 
   def quad_style
     fullscreen_below_navbar
-    size ["100%", "100%"]
     background_color UIColor.clearColor
   end
 end
--- app/screens/invoice_shot_screen.rb
+++ app/screens/invoice_shot_screen.rb
@@ -5,21 +5,6 @@ class InvoiceShotScreen < PM::Screen
     # After view is first loaded
     set_nav_bar_button :right, title: "Ausschneiden", action: :crop
     @layout = InvoiceShotLayout.new(root: self.view).build
-
-    @layout.pins.each do |pin|
-      PM.logger.debug "setting pin listener for pin #{pin}"
-      pin.on(:touch_drag_inside, :touch_drag_outside) do |pin, event|
-        touch = event.allTouches.anyObject
-        pin.center = touch.locationInView(view)
-        update_quad
-      end
-    end
-
-    update_quad
-  end
-
-  def update_quad
-    @layout.get(:quad).points = @layout.pins.map(&:center)
   end
 
   def crop
@@ -38,7 +23,7 @@ class InvoiceShotScreen < PM::Screen
     image_quad_height_ratio = Rational(image_size.height, quad_size.height.to_f)
     image_left_x = @layout.get(:invoice_image).frame.origin.x
 
-    @layout.pins.map(&:center).map do |cgpoint|
+    @layout.get(:quad).points.map do |cgpoint|
       [(cgpoint.x - image_left_x) * image_quad_height_ratio, cgpoint.y * image_quad_height_ratio]
     end
   end
